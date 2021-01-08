fceb22e413f61e58d8b582a04cffb8e4a61f3175
Nazeeh-Saifi <nazeeh.saifi@gmail.com>
2021-01-08 11:29:17 +0200

    creating api resource controllers for product and supplier

--- sawafTaskApp/routes/api.php
+++ sawafTaskApp/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SupplierController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,17 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('products',ProductController::class);
+Route::apiResource('suppliers',SupplierController::class);
+
+/* 
+Route::prefix('menu/menu')->group(function () { 
+    Route::get('/',         'MenuEditController@index')->name('menu.menu.index');
+    Route::get('/create',   'MenuEditController@create')->name('menu.menu.create');
+    Route::post('/store',   'MenuEditController@store')->name('menu.menu.store');
+    Route::get('/edit',     'MenuEditController@edit')->name('menu.menu.edit');
+    Route::post('/update',  'MenuEditController@update')->name('menu.menu.update');
+    Route::get('/delete',   'MenuEditController@delete')->name('menu.menu.delete');
+});
+ */
